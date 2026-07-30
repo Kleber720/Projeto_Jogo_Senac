380fc9d2af77867da4c5e5b7a7c72170f2743566
csrッ <99185429+CesarZanelatto@users.noreply.github.com>
2026-07-30 08:19:45 -0300

    Update menu.js

--- Script/menu.js
+++ Script/menu.js
@@ -2,11 +2,8 @@ let iniciou = false;
 
 function iniciarMenu() {
     if (iniciou) return;
-
     iniciou = true;
-
     document.getElementById("meuTexto").innerText = "";
-
     document.getElementById("menu").classList.add("menu-iniciado");
 }
 
