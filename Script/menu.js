133840c3de11604176bf02cc4703ff80ea0ae5c6
csrッ <99185429+CesarZanelatto@users.noreply.github.com>
2026-07-30 08:41:17 -0300

    feat: adiciona animações de transição ao menu inicial

--- Script/menu.js
+++ Script/menu.js
@@ -3,7 +3,6 @@ let iniciou = false;
 function iniciarMenu() {
     if (iniciou) return;
     iniciou = true;
-    document.getElementById("meuTexto").innerText = "";
     document.getElementById("menu").classList.add("menu-iniciado");
 }
 
