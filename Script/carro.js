c08cb7524a829dc0af40b6c4bcbba900b4cb266f
leandroxelor00 <leandroxelor00@gmail.com>
2026-07-31 10:02:36 -0300

    adicionando mais cores de carros

--- Script/carro.js
+++ Script/carro.js
@@ -5,10 +5,38 @@ const carro2 = document.querySelector("#carro2");
 const listaURL = [
   "../Imagem/cenario/assets/carro/carro-vermei.png",
   "../Imagem/cenario/assets/carro/carro-rosa.png",
+  "../Imagem/cenario/assets/carro/carro-amarelo.png",
+  "../Imagem/cenario/assets/carro/carro-roxo.png",
 ];
 
-const random = Math.floor(Math.random() * listaURL.length);
+const listaURLInvertidos = [
+  "../Imagem/cenario/assets/carro/carro-vermei-invertido.png",
+  "../Imagem/cenario/assets/carro/carro-rosa-invertido.png",
+  "../Imagem/cenario/assets/carro/carro-amarelo-invertido.png",
+  "../Imagem/cenario/assets/carro/carro-roxo-invertido.png",
+];
 
-const itemRandom = listaURL[random];
+function mudarCarro() {
+  const random = Math.floor(Math.random() * listaURL.length);
+  carro1.style.backgroundImage = `url(${listaURL[random]})`;
+}
 
-carro1.style.backgroundImage = `url(${itemRandom})`;
+function mudarCarroInvertido() {
+  const random = Math.floor(Math.random() * listaURLInvertidos.length);
+  carro2.style.backgroundImage = `url(${listaURLInvertidos[random]})`;
+}
+
+mudarCarro();
+mudarCarroInvertido();
+
+carro1.addEventListener("animationiteration", (e) => {
+  if (e.animationName === "carroAndar") {
+    mudarCarro();
+  }
+});
+
+carro2.addEventListener("animationiteration", (e) => {
+  if (e.animationName === "carroAndarDeRe") {
+    mudarCarroInvertido();
+  }
+});
